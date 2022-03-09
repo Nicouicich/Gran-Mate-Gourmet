92c7a0d8e8cea8d6ba0138441fcef0cba05850aa
Nicouicich <nico_uicich@hotmail.com>
2022-03-09 19:03:23 -0300

    fixes

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,17 +1,12 @@
-import { render } from "@testing-library/react";
 import React from "react";
 
-class ItemListContainer extends React.Component {
-  constructor (props) {
-    super(props)
-    this.mensaje = props.mensaje
-  }
 
-  render() {
-    return (
-    <p>{this.mensaje}</p>
-    )
-  }
+
+export default function ItemListContainer ({ mensaje}){
+  return (
+    <p>{mensaje}</p>
+
+  )
 }
 
-export default ItemListContainer
+
